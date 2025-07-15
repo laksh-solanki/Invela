10d5bd9f0e93e83200591281e262710e727d7c92
One Mark <lakshsolanki848@gmail.com>
2025-07-15 20:27:41 +0530

    re-path

--- src/components/CertificatesSwiper.vue
+++ src/components/CertificatesSwiper.vue
@@ -89,12 +89,12 @@ export default {
       creativeEffect,
       certificates: [
         {
-          image: "@/assets/photo/certificate-01.jpg",
+          image: "@/public/photo/certificate-01.jpg",
           title: "AI Master Class",
           issuer: "AI Academy",
         },
         {
-          image: "@/assets/photo/certificate-02.jpg",
+          image: "@/public/photo/certificate-02.jpg",
           title: "Front End Development ",
           issuer: "FrontEnd Masters",
         },
