c46a9c227011c308fbe4ba4ef72ee56374b102c6
One Mark <lakshsolanki848@gmail.com>
2025-07-15 19:46:15 +0530

    Add All photo in public folder

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -15,7 +15,7 @@
       </div>
       <div class="futuristic-hero-art">
         <img
-          src="/public/11.png"
+          src="/public/photo/11.png"
           alt="Futuristic Art"
         />
       </div>
