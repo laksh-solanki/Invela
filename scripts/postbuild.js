1f17093f760cf128537c16923868202709913e13
One Mark <lakshsolanki848@gmail.com>
2025-07-19 13:52:52 +0530

    \"fix: Implement robust Netlify redirects and clean up build config\"

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -4,13 +4,8 @@ const path = require('path');
 const distPath = path.resolve(__dirname, '../dist');
 const publicPhotoPath = path.resolve(__dirname, '../public/photo');
 const distPhotoPath = path.join(distPath, 'photo');
-const redirectsFilePath = path.join(distPath, '_redirects');
 
 // Copy public/photo to dist/photo
 fs.copySync(publicPhotoPath, distPhotoPath, { overwrite: true });
 
-// Create _redirects file in dist
-const redirectContent = '/* /index.html 200';
-fs.writeFileSync(redirectsFilePath, redirectContent);
-
-console.log('Postbuild script executed: photo folder copied and _redirects created.');
+console.log('Postbuild script executed: photo folder copied.');
